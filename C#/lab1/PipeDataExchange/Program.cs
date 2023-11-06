5c26f617fd578933ee86f218c519781e2cbb7ec3
Dmitriy <101661452+DmitriyAvgustinovich@users.noreply.github.com>
2023-11-06 15:14:12 +0500

    Update Program.cs

--- C#/lab1/PipeDataExchange/Program.cs
+++ C#/lab1/PipeDataExchange/Program.cs
@@ -47,3 +47,64 @@ namespace PipeServer
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+
+namespace PipeServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            using NamedPipeServerStream pipeServer = new("testpipe", PipeDirection.InOut);
+
+            Console.WriteLine("Ожидание клиента...");
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Клиент подключен.");
+
+            var data = new DataStruct { Field1 = 10, Field2 = 20 };
+            pipeServer.Write(DataStructToBytes(data));
+
+            byte[] buffer = new byte[1000];
+            int bytesRead = pipeServer.Read(buffer, 0, buffer.Length);
+            var receivedData = BytesToDataStruct(buffer);
+
+            Console.WriteLine("Получены данные от клиента:");
+            Console.WriteLine($"Field1: {receivedData.Field1}, Field2: {receivedData.Field2}");
+        }
+
+        public struct DataStruct
+        {
+            public int Field1;
+            public int Field2;
+        }
+
+        static byte[] DataStructToBytes(DataStruct data)
+        {
+            int size = Marshal.SizeOf<DataStruct>();
+            byte[] arr = new byte[size];
+            MemoryMarshal.Write(arr, ref data);
+            return arr;
+        }
+
+        static DataStruct BytesToDataStruct(byte[] bytes)
+        {
+            DataStruct data;
+            data = MemoryMarshal.Read<DataStruct>(bytes);
+            return data;
+        }
+    }
+}
+
