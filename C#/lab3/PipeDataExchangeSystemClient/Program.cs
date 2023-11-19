14e83ccb2bec5c856189e72d87efde9f297759e6
Дмитрий <Avgustinovich.DV@parmalogica.ru>
2023-11-19 22:29:23 +0500

    _

---
+++ C#/lab3/PipeDataExchangeSystemClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace PipeClient
+{
+    public struct Data
+    {
+        public int Value;
+        public bool Confirm;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Соединение с сервером...");
+
+                var pipeStream = new NamedPipeClientStream(".", "pipe", PipeDirection.InOut);
+                pipeStream.Connect();
+
+                Console.WriteLine("Соединение установлено");
+                Console.WriteLine("Ожидание данных...");
+
+                while (true)
+                {
+                    byte[] buffer = new byte[Unsafe.SizeOf<Data>()];
+                    pipeStream.Read(buffer);
+
+                    var data = MemoryMarshal.Read<Data>(buffer);
+                    Console.WriteLine($"Получено: {data.Value}, {data.Confirm}");
+
+                    data.Confirm = true;
+                    Console.WriteLine($"Отправлено: {data.Value}, {data.Confirm}");
+
+                    byte[] resBuffer = new byte[Unsafe.SizeOf<Data>()];
+                    MemoryMarshal.Write(resBuffer, ref data);
+
+                    pipeStream.Write(resBuffer);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ C#/lab3/PipeDataExchangeSystemServer/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace PipeServer
+{
+    public struct Data
+    {
+        public int Value;
+        public bool Confirm;
+
+        public override string ToString()
+        {
+            return $"Введенные данные = {Value}, Изменения = {Confirm}";
+        }
+    }
+
+    internal class Program
+    {
+        private static CancellationTokenSource cancellationTokenSource = new();
+        private static CancellationToken cancellationToken = cancellationTokenSource.Token;
+        private static PriorityQueue<Data, int> adQueue = new();
+        private static Mutex mutex = new();
+
+        private static Task ClientTask(CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Значение: ");
+                    var value = Console.ReadLine();
+
+                    Console.WriteLine("Приоритет: ");
+                    var priority = Console.ReadLine();
+
+                    var data = new Data { Value = Convert.ToInt32(value), Confirm = false };
+                    mutex.WaitOne();
+
+                    adQueue.Enqueue(data, Convert.ToInt32(priority));
+                    mutex.ReleaseMutex();
+                }
+            }, token);
+        }
+
+        private static Task ServerTask(NamedPipeServerStream pipeStream, CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                List<Data> process = new();
+
+                while (!token.IsCancellationRequested)
+                {
+                    try
+                    {
+                        if (adQueue.Count >= 1)
+                        {
+                            mutex.WaitOne();
+                            var data = adQueue.Dequeue();
+                            mutex.ReleaseMutex();
+
+                            byte[] buffer = new byte[Unsafe.SizeOf<Data>()];
+                            MemoryMarshal.Write(buffer, ref data);
+
+                            pipeStream.Write(buffer);
+
+                            byte[] resBuffer = new byte[Unsafe.SizeOf<Data>()];
+                            pipeStream.Read(resBuffer);
+
+                            process.Add(MemoryMarshal.Read<Data>(resBuffer));
+
+                            using (var fileStream = new FileStream("input.txt", FileMode.Append, FileAccess.Write))
+                            using (var streamWriter = new StreamWriter(fileStream))
+                            {
+                                streamWriter.WriteLine($"{data.Value}, {data.Confirm}");
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                foreach (var entity in process)
+                {
+                    Console.WriteLine(entity);
+                }
+            }, token);
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cancellationTokenSource.Cancel();
+            };
+
+            Console.WriteLine("Ожидание клиента...");
+
+            var pipeStream = new NamedPipeServerStream("pipe", PipeDirection.InOut);
+            pipeStream.WaitForConnection();
+
+            Console.WriteLine("Клиент подключен");
+
+            Task server = ServerTask(pipeStream, cancellationToken);
+            Task client = ClientTask(cancellationToken);
+
+            await Task.WhenAll(server, client);
+        }
+    }
+}
